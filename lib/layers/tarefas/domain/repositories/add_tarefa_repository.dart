d9235d25a27cab4046e22500cb58794d2a1c7b7c
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-03-26 09:28:24 -0300

    Adicionado o usecase para adicionar tarefas

--- lib/layers/tarefas/domain/repositories/add_tarefa_repository.dart
+++ lib/layers/tarefas/domain/repositories/add_tarefa_repository.dart
@@ -1,5 +1,5 @@
 import 'package:tarefas_clean_architecture/layers/tarefas/domain/entities/tarefa_entity.dart';
 
-abstract class AddTarefaUseCase {
-  Future<bool> call(TarefaEntity tarefaEntity);
+abstract class AddTarefaRepository {
+  Future<bool> call(TarefaEntity tarefa);
 }
--- lib/layers/tarefas/domain/usecases/add_tarefa/add_tarefa_usecase_imp.dart
+++ lib/layers/tarefas/domain/usecases/add_tarefa/add_tarefa_usecase_imp.dart
@@ -1,9 +1,14 @@
 import 'package:tarefas_clean_architecture/layers/tarefas/domain/entities/tarefa_entity.dart';
+import 'package:tarefas_clean_architecture/layers/tarefas/domain/repositories/add_tarefa_repository.dart';
 import 'package:tarefas_clean_architecture/layers/tarefas/domain/usecases/add_tarefa/add_tarefa_usecase.dart';
 
 class AddTarefaUseCaseImp implements AddTarefaUseCase {
+  final AddTarefaRepository _addTarefaRepository;
+
+  AddTarefaUseCaseImp(this._addTarefaRepository);
+
   @override
-  TarefaEntity addTarefa(TarefaEntity tarefaEntity) {
-    return tarefaEntity;
+  Future<bool> call(TarefaEntity tarefaEntity) async {
+    return await _addTarefaRepository(tarefaEntity);
   }
 }
